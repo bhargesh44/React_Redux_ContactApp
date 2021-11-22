a6d6772af3aaae2250e0fcaeb10d387502a56aae
Bhargesh <bhargeshgediya44@gmail.com>
2021-11-22 16:53:34 +0530

    final all done

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,6 +18,8 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 
+import "react-phone-number-input/style.css";
+import PhoneInput from "react-phone-number-input";
 import moment from "moment";
 import TextField from "@mui/material/TextField";
 
@@ -27,7 +29,7 @@ import DatePicker from "@mui/lab/DatePicker";
 const useStyles = makeStyles({
   table: {
     width: "80%",
-    margin: "50px 0 0 100px",
+    margin: "50px 0 0 175px",
   },
   thead: {
     "& > *": {
@@ -42,23 +44,35 @@ const useStyles = makeStyles({
     },
   },
   container: {
-    width: "10%",
+    width: "105%",
     margin: "0 0 0 0",
     "& > *": {
-      marginRight: 20,
+      marginRight: 30,
+      marginLeft: 30,
     },
+  },
+  button: {
+    width: "33%",
+    margin: "10px 0 0 0",
+    "& > *": {},
   },
 });
 const Home = () => {
   const classes = useStyles();
   const [searchTerm, setSearchTerm] = useState("");
+  const [phoneSearch, setPhoneSearch] = useState("");
 
   const [birthDate, setBirthDate] = useState(null);
   const clearDate = () => {
     setBirthDate(null);
   };
+  const clearPhoneNumber = () => {
+    setPhoneSearch("");
+  };
+
   const contacts = useSelector((state) => state);
   const dispatch = useDispatch();
+
   const deleteContact = (id) => {
     dispatch({ type: "DELETE_CONTACT", payload: id });
     toast.success(`Contact deleted successfully!!!`);
@@ -68,55 +82,92 @@ const Home = () => {
     <>
       <Grid
         container
+        spacing={2}
         direction="row"
-        display="flex"
-        justifyContent="center"
-        style={{ marginTop: 25 }}
+        style={{ margin: "50px 0 0 175px", width: "80%" }}
       >
-        <FormGroup className={classes.container}>
-          <FormControl>
-            <InputLabel htmlFor="my-input">Search By Name</InputLabel>
+        <Grid item lg={8} md={12} sm={12} xs={12}>
+          <Grid container>
+            <Grid item>
+              <FormGroup className={classes.container}>
+                <FormControl>
+                  <InputLabel htmlFor="my-input">
+                    Search By Name And Email
+                  </InputLabel>
+                  <Input
+                    onChange={(event) => {
+                      setSearchTerm(event.target.value);
+                    }}
+                  />
+                </FormControl>
+              </FormGroup>
+            </Grid>
+            <Grid item>
+              <FormControl className={classes.container}>
+                <LocalizationProvider dateAdapter={AdapterDateFns}>
+                  <DatePicker
+                    label=" Search By Birth Date"
+                    value={birthDate}
+                    inputVariant="outlined"
+                    onChange={(newBirthDate) => {
+                      setBirthDate(moment(newBirthDate).format("YYYY-MM-DD"));
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
 
-            <Input
-              onChange={(event) => {
-                setSearchTerm(event.target.value);
-              }}
-            />
-          </FormControl>
-        </FormGroup>
-        <LocalizationProvider
-          dateAdapter={AdapterDateFns}
-          style={{ marginTop: 20 }}
-        >
-          <DatePicker
-            label=" Search By Birth Date"
-            value={birthDate}
-            inputVariant="outlined"
-            onChange={(newBirthDate) => {
-              setBirthDate(moment(newBirthDate).format("YYYY-MM-DD"));
-            }}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </LocalizationProvider>
-        <Button
-          color="secondary"
-          variant="contained"
-          title="Clear Date"
-          onClick={clearDate}
-        >
-          {" "}
-          Clear Date{" "}
-        </Button>
-        <Button
-          color="primary"
-          variant="contained"
-          title="Add Contact"
-          component={Link}
-          to={`/add`}
-          style={{ marginTop: 20, marginLeft: 750 }}
-        >
-          Add Contact
-        </Button>
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  title="Clear Date"
+                  onClick={clearDate}
+                  style={{ marginTop: 10 }}
+                >
+                  Clear Date
+                </Button>
+              </FormControl>
+            </Grid>
+            <Grid item>
+              <FormControl className={classes.container}>
+                {/* <InputLabel>Search By Phone Number</InputLabel>
+                <Input
+                  type="number"
+                  onChange={(event) => {
+                    setPhoneSearch(event.target.value);
+                  }}
+                /> */}
+                <PhoneInput
+                  placeholder="Search By Phone Number"
+                  value={phoneSearch}
+                  onChange={setPhoneSearch}
+                  style={{ marginTop: 20 }}
+                />
+                <Button
+                  color="secondary"
+                  variant="contained"
+                  title="Clear Date"
+                  onClick={clearPhoneNumber}
+                  style={{ marginTop: 10 }}
+                >
+                  Clear PhoneNumber
+                </Button>
+              </FormControl>
+            </Grid>
+          </Grid>
+        </Grid>
+
+        <Grid item lg={4} md={12} sm={12} xs={12}>
+          <Button
+            className={classes.button}
+            color="primary"
+            variant="contained"
+            title="Add Contact"
+            component={Link}
+            to={`/add`}
+          >
+            Add Contact
+          </Button>
+        </Grid>
       </Grid>
       <Table className={classes.table}>
         <TableHead>
@@ -132,14 +183,27 @@ const Home = () => {
         <TableBody>
           {contacts
             .filter((contact) => {
-              if (searchTerm === "" && birthDate === null) {
+              if (
+                searchTerm === "" &&
+                birthDate === null &&
+                phoneSearch === ""
+              ) {
                 return contact;
               } else if (
-                (contact.birthDate.includes(birthDate) || birthDate === null )&&
-                contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+                (contact.birthDate.includes(birthDate) || birthDate === null) &&
+                (contact.name
+                  .toLowerCase()
+                  .includes(searchTerm.toLowerCase()) ||
+                  contact.email
+                    .toLowerCase()
+                    .includes(searchTerm.toLowerCase()) ||
+                  searchTerm === "") &&
+                (contact.phone.includes(phoneSearch) ||
+                  contact.phoneSearch === "")
               ) {
                 return contact;
               }
+              return false;
             })
             .map((contact, id) => (
               <TableRow className={classes.row} key={id}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,29 @@
-import {AppBar, makeStyles, Toolbar} from "@material-ui/core"
+import { AppBar, makeStyles, Toolbar } from "@material-ui/core";
 import { NavLink } from "react-router-dom";
 
 const useStyle = makeStyles({
-    header: {
-        background: "#111111"
-    },
-    tabs:{
-        color:"#FFFFFF",
-        textDecoration:"none",
-        marginLeft:50,
-        fontSize: 25
-    }
-})
+  header: {
+    background: "#111111",
+  },
+  tabs: {
+    color: "#FFFFFF",
+    textDecoration: "none",
+    marginLeft: 50,
+    fontSize: 25,
+  },
+});
 
 const Navbar = () => {
-    const classes = useStyle();
-    return(
-        <AppBar className={classes.header} position="static">
-            <Toolbar>
-                <NavLink className={classes.tabs} to="/" exact >React Redux Contact App</NavLink>
-            </Toolbar>
-        </AppBar>
-    )
-}
-
+  const classes = useStyle();
+  return (
+    <AppBar className={classes.header} position="static">
+      <Toolbar>
+        <NavLink className={classes.tabs} to="/">
+          React Redux Contact App
+        </NavLink>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
 export default Navbar;
